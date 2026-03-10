723e6e06ba82225552e56754ecd23b9007806b1a
goczangabor24 <goczangabor24@gmail.com>
2026-03-10 13:18:46 +0100

    Update po_match.py

--- po_match.py
+++ po_match.py
@@ -217,8 +217,8 @@ def render_interactive_dn_table(df: pd.DataFrame) -> None:
     components.html(html_block, height=600, scrolling=True)
 
 
-st.set_page_config(page_title="PO Collector", page_icon="📋", layout="wide")
-st.title("PO Collector")
+st.set_page_config(page_title="🐶 PO Match", page_icon="📋", layout="wide")
+st.title("🐶 PO Match")
 
 col1, col2 = st.columns(2)
 with col1:
@@ -258,3 +258,4 @@ else:
         file_name="po_dn_status_by_fc.csv",
         mime="text/csv",
     )
+
